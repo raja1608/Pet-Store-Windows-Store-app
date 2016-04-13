525226dd96969f73efcb5bd67dae1a4b02f19b70
Raja Thiraviam <rajathiraviam@hotmail.com>
2016-04-14 10:48:05 +1200

    first commit

---
+++ FriendsWithPaws/App.xaml.cs
@@ -0,0 +1,206 @@
+﻿using FriendsWithPaws.Common;
+using FriendsWithPaws.Data;
+using Windows.ApplicationModel.Search;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ApplicationSettings;
+using Windows.UI.Popups;
+using Windows.Storage;
+
+// The Grid App template is documented at http://go.microsoft.com/fwlink/?LinkId=234226
+
+namespace FriendsWithPaws
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton Application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        void OnSuggestionsRequested(SearchPane sender, SearchPaneSuggestionsRequestedEventArgs args)
+        {
+            string query = args.QueryText.ToLower();
+            string[] terms = { "beagle", "yorkshire terrier", "basset hound", "australian shepherd" };
+            List<string> termList = new List<string>() { "beagle", "yorkshire terrier", "basset hound", "australian shepherd" };
+
+            foreach (var term in termList)
+            {
+                if (term.StartsWith(query)) args.Request.SearchSuggestionCollection.AppendQuerySuggestion(term);
+            }
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used when the application is launched to open a specific file, to display
+        /// search results, and so forth.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override async void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+
+            if (args.PreviousExecutionState == ApplicationExecutionState.Running)
+            {
+                if (!string.IsNullOrEmpty(args.Arguments)) ((Frame)Window.Current.Content).Navigate(typeof(ItemDetailPage), args.Arguments);
+                Window.Current.Activate();
+                return;
+            }
+            
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+                //Associate the frame with a SuspensionManager key                                
+                SuspensionManager.RegisterFrame(rootFrame, "AppFrame");
+
+                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    // Restore the saved session state only when appropriate
+                    try
+                    {
+                        await SuspensionManager.RestoreAsync();
+                    }
+                    catch (SuspensionManagerException)
+                    {
+                        //Something went wrong restoring state.
+                        //Assume there is no state and continue
+                    }
+                }
+
+                if (args.PreviousExecutionState == ApplicationExecutionState.Running)
+                {
+                    Window.Current.Activate();
+                    return;
+                }
+                await BreedDataSource.LoadDataAsync();
+
+                SearchPane.GetForCurrentView().SuggestionsRequested += OnSuggestionsRequested;
+                // SearchPane.GetForCurrentView().SearchHistoryEnabled = false;
+               // SettingsPane.GetForCurrentView().CommandsRequested += OnCommandsRequested;
+
+                if (!string.IsNullOrEmpty(args.Arguments))
+                {
+                    rootFrame.Navigate(typeof(ItemDetailPage), args.Arguments);
+                    Window.Current.Content = rootFrame;
+                    Window.Current.Activate();
+                    return;
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                if (!rootFrame.Navigate(typeof(GroupedItemsPage), "AllGroups"))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+   
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private async void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await SuspensionManager.SaveAsync();
+            deferral.Complete();
+        }
+
+        /// <summary>
+        /// Invoked when the application is activated to display search results.
+        /// </summary>
+        /// <param name="args">Details about the activation request.</param>
+        protected async override void OnSearchActivated(Windows.ApplicationModel.Activation.SearchActivatedEventArgs args)
+        {
+            if (args.PreviousExecutionState == ApplicationExecutionState.NotRunning || args.PreviousExecutionState == ApplicationExecutionState.ClosedByUser
+                || args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+            {
+                await BreedDataSource.LoadDataAsync();
+                SearchPane.GetForCurrentView().SuggestionsRequested += OnSuggestionsRequested;
+            }
+            // TODO: Register the Windows.ApplicationModel.Search.SearchPane.GetForCurrentView().QuerySubmitted
+            // event in OnWindowCreated to speed up searches once the application is already running
+
+            // If the Window isn't already using Frame navigation, insert our own Frame
+            var previousContent = Window.Current.Content;
+            var frame = previousContent as Frame;
+
+            // If the app does not contain a top-level frame, it is possible that this 
+            // is the initial launch of the app. Typically this method and OnLaunched 
+            // in App.xaml.cs can call a common method.
+            if (frame == null)
+            {
+                // Create a Frame to act as the navigation context and associate it with
+                // a SuspensionManager key
+                frame = new Frame();
+                FriendsWithPaws.Common.SuspensionManager.RegisterFrame(frame, "AppFrame");
+
+                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    // Restore the saved session state only when appropriate
+                    try
+                    {
+                        await FriendsWithPaws.Common.SuspensionManager.RestoreAsync();
+                    }
+                    catch (FriendsWithPaws.Common.SuspensionManagerException)
+                    {
+                        //Something went wrong restoring state.
+                        //Assume there is no state and continue
+                    }
+                }
+            }
+
+            frame.Navigate(typeof(SearchResultsPage), args.QueryText);
+            Window.Current.Content = frame;
+
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        public void applicationsettings()
+        {
+            ApplicationDataContainer localsettings = ApplicationData.Current.LocalSettings;
+            localsettings.Values["simplesettings"] = "Simple settings example";
+        }
+       
+    }
+}
---
+++ FriendsWithPaws/ItemDetailPage.xaml.cs
@@ -0,0 +1,146 @@
+﻿using FriendsWithPaws.Data;
+using Windows.UI.StartScreen;
+using Windows.UI.Notifications;
+using Windows.UI.Popups;
+using Windows.Data.Xml.Dom;
+using Windows.ApplicationModel.DataTransfer;
+using System.Text;
+using Windows.Storage.Streams;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Item Detail Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234232
+
+namespace FriendsWithPaws
+{
+    /// <summary>
+    /// A page that displays details for a single item within a group while allowing gestures to
+    /// flip through other items belonging to the same group.
+    /// </summary>
+    public sealed partial class ItemDetailPage : FriendsWithPaws.Common.LayoutAwarePage
+    {
+        public ItemDetailPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // Allow saved page state to override the initial item to display
+            if (pageState != null && pageState.ContainsKey("SelectedItem"))
+            {
+                navigationParameter = pageState["SelectedItem"];
+            }
+
+            // TODO: Create an appropriate data model for your problem domain to replace the sample data
+            var item = BreedDataSource.GetItem((String)navigationParameter);
+            this.DefaultViewModel["Group"] = item.Group;
+            this.DefaultViewModel["Items"] = item.Group.Items;
+            this.flipView.SelectedItem = item;
+
+            DataTransferManager.GetForCurrentView().DataRequested += OnDataRequested;
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+            var selectedItem = (BreedDataItem)this.flipView.SelectedItem;
+            pageState["SelectedItem"] = selectedItem.UniqueId;
+
+            DataTransferManager.GetForCurrentView().DataRequested -= OnDataRequested;
+        }
+
+        private async void OnPinBookButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var item = (BreedDataItem)this.flipView.SelectedItem;
+            var uri = new Uri(item.TileImagePath.AbsoluteUri);
+            var tile = new SecondaryTile(item.UniqueId, item.ShortTitle, item.Title,item.UniqueId, TileOptions.ShowNameOnLogo, uri);
+            await tile.RequestCreateAsync();
+
+        }
+
+        private async void OnReminderButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var notifier = ToastNotificationManager.CreateToastNotifier();
+            if (notifier.Setting != NotificationSetting.Enabled)
+            {
+                var dialog = new MessageDialog("Notifications are currently disabled");
+                await dialog.ShowAsync();
+                return;
+            }
+
+            var template = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+            var element = template.GetElementsByTagName("text")[0];
+            element.AppendChild(template.CreateTextNode("Reminder! Check FriendsWithPaws"));
+            var date = DateTimeOffset.Now.AddSeconds(15);
+            var stn = new ScheduledToastNotification(template, date);
+            notifier.AddToSchedule(stn);
+            
+
+            var template1 = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText01);
+            var element1 = template1.GetElementsByTagName("text");
+            element1[0].AppendChild(template1.CreateTextNode("Please check what's new with Friends With Paws"));
+            
+            var image = template1.GetElementsByTagName("image")[0] as XmlElement;
+            image.SetAttribute("src", "ms-appx:///Assets/Logo.png");
+            image.SetAttribute("alt", "FriendsWithPaws");
+
+            var toastNode = template1.SelectSingleNode("/toast");
+            ((XmlElement)toastNode).SetAttribute("duration", "long");
+
+            var date1 = DateTimeOffset.Now.AddSeconds(20);
+            var stn1 = new ScheduledToastNotification(template1, date1);
+            notifier.AddToSchedule(stn1);
+
+        }
+
+        void OnDataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            var request = args.Request;
+            
+            var item = (BreedDataItem)this.flipView.SelectedItem;
+
+            request.Data.Properties.Title = item.Title;
+            request.Data.Properties.Description = "Breed details and description";
+
+            var breed = "\r\nBread Details\r\n";
+            breed += string.Join("\r\n", item.Breed_details);
+            breed += ("\r\n\r\nDescription\r\n" + item.Description);
+
+            request.Data.SetText(breed);
+
+            var reference = RandomAccessStreamReference.CreateFromUri(new Uri(item.ImagePath.AbsoluteUri));
+
+            request.Data.Properties.Thumbnail = reference;
+            request.Data.SetBitmap(reference);
+           // request.Data.SetHtmlFormat(HtmlFormatHelper.CreateHtmlFormat("test---"));
+        }
+
+    }
+}
